4dc04e163ec29db3cd67d5b54709650774edda0a
Vrushali Joshi <vrushalijoshi589@gmail.com>
2023-06-16 16:18:32 +0530

    example of Await Function

--- AsynchAndAwait.js
+++ AsynchAndAwait.js
@@ -10,3 +10,24 @@ async function f() {
 f().then(function(result) {
     console.log(result)
 });
+
+// Await function
+// a promise
+let promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+    resolve('Promise resolved')}, 4000); 
+});
+
+// async function
+async function asyncFunc() {
+
+    //he await keyword waits for the promise to be complete (resolve or reject).
+    // wait until the promise resolves 
+    let result = await promise; 
+
+    console.log(result);
+    console.log('hello');
+}
+
+// calling the async function
+asyncFunc();
